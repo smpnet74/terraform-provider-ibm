1a58e1350ca7a917fef68165ef7942d807ee4aa1
Scott Peterson <sm_peterson@yahoo.com>
2021-04-15 13:05:11 -0500

    commit

--- examples/ibm-cluster/vpc-gen2-cluster/main.tf
+++ examples/ibm-cluster/vpc-gen2-cluster/main.tf
@@ -22,7 +22,7 @@ resource "ibm_is_subnet" "subnet1" {
   total_ipv4_address_count = 256
 }
 
-resource "ibm_is_subnet1" "subnet2" {
+resource "ibm_is_subnet" "subnet2" {
   name                     = "subnet-${random_id.name2.hex}"
   vpc                      = ibm_is_vpc.vpc1.id
   zone                     = local.ZONE2
