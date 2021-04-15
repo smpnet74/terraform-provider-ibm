269d6c5a599390ddff606f73a010b1177c0049ba
Scott Peterson <sm_peterson@yahoo.com>
2021-04-15 12:57:58 -0500

    again

--- examples/ibm-cluster/vpc-gen2-cluster/main.tf
+++ examples/ibm-cluster/vpc-gen2-cluster/main.tf
@@ -12,18 +12,18 @@ locals {
 }
 
 resource "ibm_is_vpc" "vpc1" {
-  name = "vpc-${random_id.name1.dec}"
+  name = "vpc-${random_id.name1.hex}"
 }
 
 resource "ibm_is_subnet" "subnet1" {
-  name                     = "subnet-${random_id.name1.dec}"
+  name                     = "subnet-${random_id.name1.hex}"
   vpc                      = ibm_is_vpc.vpc1.id
   zone                     = local.ZONE1
   total_ipv4_address_count = 256
 }
 
-resource "ibm_is_subnet" "subnet2" {
-  name                     = "subnet-${random_id.name2.dec}"
+resource "ibm_is_subnet1" "subnet2" {
+  name                     = "subnet-${random_id.name2.hex}"
   vpc                      = ibm_is_vpc.vpc1.id
   zone                     = local.ZONE2
   total_ipv4_address_count = 256
